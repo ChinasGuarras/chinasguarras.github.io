573c6eb29efd72a26eec6151dfef9240748ced26
MN Entertainment <muchosnegrosoficial@gmail.com>
2026-01-10 09:44:59 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const countdownTarget = new Date(2026, 0, 10, 0, 0, 0);
+const countdownTarget = new Date(2026, 1, 14, 0, 0, 0);
 
 function updateCountdown() {
     const now = new Date().getTime();
